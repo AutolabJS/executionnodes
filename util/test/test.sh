723c89fc94e0cba693226741c7e5db5e1967eb44
prasadtalasila <prasad.talasila@eng.au.dk>
2019-01-17 18:55:31 +0100

    Adds the missing util/ component
      Execution node uses util/ component, but this component has not
      been added. The problem is rectified now.

---
+++ util/test/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+############
+# Authors: Ankshit Jain
+# Purpose: To run all different types of tests on util modules
+# Invocation: $bash test.sh
+# Date: 01-Feb-2018
+# Previous Versions: None
+###########
+# All variables that are exported/imported are in upper case convention. They are:
+#  TEST_TYPE : determines which tests are to be run
+#    Valid values are: "UNIT", "FUNCTION", "INTEGRATION", "THROUGHPUT"
+
+set -ex
+if [[ -f package.json ]]
+then
+  case $TEST_TYPE in
+    "UNIT")
+      mocha -u bdd -R tap ./test/unit ;;
+    "FUNCTION")
+      # DO NOTHING FOR NOW
+      : ;;
+    "INTEGRATION")
+      # DO NOTHING FOR NOW
+      : ;;
+    "THROUGHPUT")
+      # DO NOTHING FOR NOW
+      : ;;
+    *)
+      echo "The specified test type $TEST_TYPE is not available. Exiting."
+      exit 1;;
+  esac
+fi
